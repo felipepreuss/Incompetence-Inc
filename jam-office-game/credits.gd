dbcd76ac7f5783cf1bdc7c529c8cef05a606b71b
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-10-30 18:28:04 -0300

    Add files via upload

--- jam-office-game/credits.gd
+++ jam-office-game/credits.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$credcredre.play() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,17 @@ func _process(delta: float) -> void:
 
 
 func _on_return_menu_pressed() -> void:
+	 
 	get_tree().change_scene_to_file("res://Scenes/menumain.tscn") # Replace with function body.
+	$credcredre.stop()
+	
+
+func _on_return_menu_mouse_entered() -> void:
+	$VBoxContainer/Return_menu/HoverC.play() # Replace with function body.
+
+ 
+# Replace with function body.
+
+
+func _on_return_menu_focus_entered() -> void:
+	$VBoxContainer/Return_menu/PressedC.play() # Replace with function body.
