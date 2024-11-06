5e54eb8a972af10ef2cfc7f4668b61e19caec224
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-11-06 20:03:07 -0300

    Add files via upload

--- jam-office-game/Scenes/Global.gd
+++ jam-office-game/Scenes/Global.gd
@@ -1,5 +1,5 @@
 extends Node
-var declared_values = [100, 200, 500, 1000, 1500]
+var declared_values = [1, 5, 10, 20, 50, 100, 200, 500, 1000, 1500]
 #@onready var moneycash = $Money
 var dinheiro = 0
 var valor:int
